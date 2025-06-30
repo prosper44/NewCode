0e55bd6599240ca2bde540cc852e66bc2b2afecd
obinna <michaelobinna35@gmail.com>
2025-06-30 20:29:11 +0100

    Final cleanup: Tailwind + router fix

--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter, RouterProvider } from 'react-router-dom'
-import Gallery from './pages/Gallery.jsx'
-
-
-
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
-
-   
+  <StrictMode>
     <BrowserRouter>
-       <App />
+      <App />
     </BrowserRouter>
-  
+  </StrictMode>
 )
